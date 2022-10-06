a4249f52aa4a94c3c81b317900e7bf95b89e6aa6
jil249 <103066654+jil249@users.noreply.github.com>
2022-10-06 10:41:33 -0800

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,3 +1,25 @@
 import java.io.IOException;
 import java.net.URI;
 
+class Handler implements URLHandler {
+    String s ="";
+
+
+    public String handleRequest(URI url){
+        if(url.getPath().contains("/add")){
+            String[] parameters = url.getQuery().split("=");
+            if(parameters[0].equals("s")){
+                s += (" ") += String.parseStr(parameters[1]);
+                return String.format("The word %s is added! It's now %d",parameters[1], s );
+            }
+        }
+        else if (url.getPath().contains("/search")){
+
+
+        }
+        else{
+            return "404 Not Found!";
+        }
+    }   
+}
+
