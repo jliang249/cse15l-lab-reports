e840bae293c31b633de4c8e444fa7098fbf6a27a
jliang249 <jiayi17liang@gmail.com>
2022-10-12 23:49:20 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,14 +2,14 @@ import java.io.IOException;
 import java.net.URI;
 
 class SearchEngine implements NumberServer {
-    String s ="";
+    String [] sArray = new String [] {" "};
 
 
     public String handleRequest(URI url){
         if(url.getPath().contains("/add")){
             String[] parameters = url.getQuery().split("=");
             if(parameters[0].equals("s")){
-                s += (" ") += String.parseStr(parameters[1]);
+                sArray += (" ") += String.parseStr(parameters[1]);
                 return String.format("The word %s is added! It's now %d",parameters[1], s );
             }
         }
