0896c447d3882af516ea0438f2f41863a50bc0b1
jliang249 <jiayi17liang@gmail.com>
2022-10-14 00:36:53 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.*;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
     // various requests.
     int num = 0;
+    List<String> str = new ArrayList<String>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
@@ -26,12 +28,12 @@ class Handler implements URLHandler {
                 }
             }
             else if(url.getPath().contains("/search")){
-                List<String> temp = new ArrayList<String>();
+                String templist = "";
                 String [] parameters = url.getQuery().split("=");
                 if(parameters[0].equals("s")){
-                    for(int i = 0; i<list.size(); i++){
-                        if(list.get(i).contains(parameters[1])){
-                            temp.add(list.get(i) + ", ");
+                    for(int i = 0; i< str.size(); i++){
+                        if(str.get(i).contains(parameters[1])){
+                            templist = templist + str.get(i) + ", ";
                         }
                     }
                 }
